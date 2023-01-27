4b3c7c1e988d3ec365b4dca90f0a042f5fe0345b
Amy Pearson <amy.pearson@aptbacs.co.uk>
2023-01-27 20:03:52 +0000

    Add project files.

---
+++ AmyPearsonVocala/Controllers/HotelsController.cs
@@ -0,0 +1,78 @@
+﻿using AmyPearsonVocala.Models;
+using AmyPearsonVocala.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace AmyPearsonVocala.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HotelsController : ControllerBase
+    {
+        private IBookingService _bookings;
+
+        public HotelsController(IBookingService bookings)
+        {
+            _bookings = bookings;
+
+        }
+
+        [HttpPost()]
+        [Route("/ConfirmHotelBooking")]
+        public async Task<ActionResult<BookingRequest>> ConfirmHotelBooking([FromBody] BookingRequest bookingRequest)
+        {
+            if (bookingRequest == null || !ModelState.IsValid)
+            {
+                return BadRequest("Invalid Booking Request");
+            }
+            if (bookingRequest.Id > 0)
+            {
+                try
+                {
+                    _bookings.UpdateBooking(bookingRequest);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest("Error updating booking: " + ex.Message);
+                }
+            }
+            else
+            {
+                try
+                {
+                    _bookings.CreateBooking(bookingRequest);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest("Error creating booking: " + ex.Message);
+                }
+            }
+
+            return Ok(bookingRequest);
+        }
+
+        [HttpGet()]
+        [Route("/GetHotelBookings")]
+        public async Task<ActionResult<IEnumerable<BookingRequest>>> GetHotelBookings()
+        {
+            try
+            {
+                var response = _bookings.GetAllBookings();
+
+                if (response == null)
+                {
+                    return NotFound("No bookings found");
+                }
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error retrieving bookings: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ AmyPearsonVocala/Services/BookingService.cs
@@ -0,0 +1,58 @@
+﻿using AmyPearsonVocala.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmyPearsonVocala.Services
+{
+    public interface IBookingService
+    {
+        public IEnumerable<BookingRequest> GetAllBookings();
+        public void UpdateBooking(BookingRequest booking);
+        public void CreateBooking(BookingRequest booking);
+
+    }
+    public class BookingService : IBookingService
+    {
+        private readonly HotelManagementContext _context;
+        public BookingService(HotelManagementContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<BookingRequest> GetAllBookings()
+        {
+           
+                var list = from Bookings in _context.Bookings
+                           select Bookings;
+
+                var bookings = list.ToList();
+                return bookings;
+           
+        }
+        public void UpdateBooking(BookingRequest booking)
+        {
+            var existingBooking = _context.Bookings.FirstOrDefault(x => x.Id == booking.Id);
+
+            if (existingBooking == null)
+            {
+                throw new Exception("Booking not found.");
+            }
+            else
+            {
+                existingBooking.HotelName = booking.HotelName;
+                existingBooking.BookingDate = booking.BookingDate;
+                existingBooking.Cost = booking.Cost;
+                existingBooking.Status = booking.Status;
+
+                _context.SaveChanges(); 
+            }
+        }
+        public void CreateBooking(BookingRequest booking)
+        {
+            _context.Bookings.Add(booking);
+            _context.SaveChanges();
+        }
+    }
+
+}
